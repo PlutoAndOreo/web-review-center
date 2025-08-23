cc320203f0cf8cbe93db8f64cf8a5674e3ff8f63
PlutoAndOreo <quitorio.ericka1997@gmail.com>
2025-08-23 08:27:46 +0800

    [ADD] set-up laravel breeze

--- web-review-center/routes/web.php
+++ web-review-center/routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
-use  App\Http\Controllers\Student\StudentVideoController;
-use  App\Http\Controllers\Admin\VideoController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-    Route::get('/', [VideoController::class, 'index']);
-    Route::post('/upload-video', [VideoController::class, 'upload']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-    Route::get('/video', [StudentVideoController::class, 'index']);
-    Route::get('/video-chunk', [StudentVideoController::class, 'stream'])->name('video.show');
-    Route::get('/video-file-size', [StudentVideoController::class, 'getVideoFileSize']);
-    Route::get('/video/{filename}', [StudentVideoController::class, 'show']);
-
+require __DIR__.'/auth.php';
--- web-review-center/vite.config.js
+++ web-review-center/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +7,12 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
+    server: {
+        host: '0.0.0.0', // allow access from Docker host
+        port: 3000,       // must match Docker port mapping
+        hmr: {
+            host: 'localhost', // host for hot module reload
+        },
+    },
 });
