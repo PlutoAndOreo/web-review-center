9122730c9fa1091ed0a88a8b5164fd12ccb39116
PlutoAndOreo <quitorio.ericka1997@gmail.com>
2025-12-24 02:42:13 +0800

    add request in app

--- web-review-center/bootstrap/app.php
+++ web-review-center/bootstrap/app.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Foundation\Configuration\Exceptions;
-
 use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
