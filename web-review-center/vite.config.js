425515a5615e29d69042a79b113d3f338ec8a921
PlutoAndOreo <quitorio.ericka1997@gmail.com>
2025-12-24 03:12:37 +0800

    add custom css and js

--- web-review-center/vite.config.js
+++ web-review-center/vite.config.js
@@ -4,7 +4,15 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/css/admin/notifications.css',
+                'resources/css/student/comments.css',
+                'resources/js/app.js',
+                'resources/js/admin/notifications.js',
+                'resources/js/admin/video-list.js',
+                'resources/js/admin/video-upload.js'
+            ],
             refresh: true,
         }),
     ],
